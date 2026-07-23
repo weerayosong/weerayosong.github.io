594e97520e3916fdfc717bc2009980a2d721edfa
weerayosong <weerayosong@outlook.com>
2026-07-23 14:12:47 +0700

    refactor: extract projects and certs data into separate modules

---
+++ data/certs.js
@@ -0,0 +1,74 @@
+export const certsData = [
+    {
+        title: "Junior Software Developer Bootcamp",
+        issuer: "Generation TH",
+        icon: "fa-globe fas",
+        bgImage:
+            "https://thailand.generation.org/wp-content/uploads/2024/08/Final-Junior-Software-Developer.png",
+        link: "https://thailand.generation.org/programs/junior-software-developer/",
+    },
+    {
+        title: "HTML CSS Masterful",
+        issuer: "MilerDev",
+        icon: "fa-html5 fab",
+        bgImage:
+            "https://github.com/weerayosong/weerayosong.github.io/blob/581515a9d7e528f430eeba8c9f61f21b77de30ea/images/htmlcss.png?raw=true",
+        link: "https://milerdev.com/certificate/CERT-RXC6-RW2P",
+    },
+    {
+        title: "Figma to Code",
+        issuer: "MilerDev",
+        icon: "fa-css3-alt fab",
+        bgImage:
+            "https://github.com/weerayosong/weerayosong.github.io/blob/581515a9d7e528f430eeba8c9f61f21b77de30ea/images/figmacode.png?raw=true",
+        link: "https://milerdev.com/certificate/CERT-RR6T-RCPA",
+    },
+    {
+        title: "JavaScript Mastery",
+        issuer: "MilerDev",
+        icon: "fa-js fab",
+        bgImage:
+            "https://github.com/weerayosong/weerayosong.github.io/blob/581515a9d7e528f430eeba8c9f61f21b77de30ea/images/jsmastery.png?raw=true",
+        link: "https://milerdev.com/certificate/CERT-FLXX-FTQU",
+    },
+    {
+        title: "ReactJS Front-End Mastery",
+        issuer: "MilerDev",
+        icon: "fa-react fab",
+        bgImage:
+            "https://github.com/weerayosong/weerayosong.github.io/blob/581515a9d7e528f430eeba8c9f61f21b77de30ea/images/reactfrontend.png?raw=true",
+        link: "https://milerdev.com/certificate/CERT-4325-QVED",
+    },
+    {
+        title: "The Complete JavaScript Course 2025: From Zero to Expert!",
+        issuer: "Jonas Schmedtmann",
+        icon: "fa-js fab",
+        bgImage:
+            "https://github.com/weerayosong/weerayosong.github.io/blob/main/images/ud1.png?raw=true",
+        link: "https://www.udemy.com/certificate/UC-f0e171d2-8c7b-4ac8-8fba-0732aea0b2fa/",
+    },
+    {
+        title: "The Ultimate React Course 2025: React, Next.js, Redux & More",
+        issuer: "Jonas Schmedtmann",
+        icon: "fa-react fab",
+        bgImage:
+            "https://github.com/weerayosong/weerayosong.github.io/blob/main/images/ud2.png?raw=true",
+        link: "https://www.udemy.com/certificate/UC-3ef70702-3c40-4e21-9c93-67af3477b4c9/",
+    },
+    {
+        title: "Node.js, Express, MongoDB & More: The Complete Bootcamp",
+        issuer: "Jonas Schmedtmann",
+        icon: "fa-node fab",
+        bgImage:
+            "https://github.com/weerayosong/weerayosong.github.io/blob/main/images/ud3.png?raw=true",
+        link: "https://www.udemy.com/certificate/UC-7f576566-1de9-4ce6-85a3-2fba8e2133bc/",
+    },
+    {
+        title: "Dev Lab",
+        issuer: "Born to Dev",
+        icon: "fa-code fas",
+        bgImage:
+            "https://www.borntodev.com/devlab/assets/devlab_logo-FRL3p2Tn.png",
+        link: "https://www.borntodev.com/devlab/certificate/619c61ae0cc9c14c874f9498a89a6475",
+    },
+];
